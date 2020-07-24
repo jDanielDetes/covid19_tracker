a4c68c8201922db4c85f393b47f12d03fc90d8b7
jDanielDetes <jdanieldetes@gmail.com>
2020-07-24 13:14:06 -0400

    startup done

---
+++ src/components/DataCharts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DataCharts extends Component {
+    render() {
+        return (
+            <div>
+                <p>hi</p>
+            </div>
+        )
+    }
+}
---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import DataCharts from '../components/DataCharts'
+
+export default class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <DataCharts/>
+            </div>
+        )
+    }
+}
