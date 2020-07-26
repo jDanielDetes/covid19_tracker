c9157365340259797ba5f32114e26733625f30f1
jDanielDetes <jdanieldetes@gmail.com>
2020-07-26 12:22:25 -0400

    leaflet map added

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import DataCharts from '../components/DataCharts'
 import '../mainPage.css'
 import LiveCasesChart from '../components/LiveCasesChart'
+import MapCases from '../components/MapCases'
 
 export default class MainPage extends Component {
     render() {
         return (
             <div>
-               <LiveCasesChart/>
+               <MapCases/>
             </div>
         )
     }
