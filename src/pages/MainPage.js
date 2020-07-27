cec968a4e1e4af2b33bbce75abee39aeb81fb951
jDanielDetes <jdanieldetes@gmail.com>
2020-07-27 11:40:14 -0400

    Map component built

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import DataCharts from '../components/DataCharts'
-import '../mainPage.css'
-import LiveCasesChart from '../components/LiveCasesChart'
-import MapCases from '../components/MapCases'
+import React, { Component, useState } from "react";
+import DataCharts from "../components/DataCharts";
+import "../mainPage.css";
+import LiveCasesChart from "../components/LiveCasesChart";
+import Map from "../components/Map";
+import "leaflet/dist/leaflet.css";
 
-export default class MainPage extends Component {
-    render() {
-        return (
-            <div>
-               <MapCases/>
-            </div>
-        )
-    }
+function MainPage() {
+const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796})
+const [mapZoom,setMapZoom] = useState(3)
+
+  return (
+    <div>
+      <Map center={mapCenter} zoom={mapZoom} />
+    </div>
+  );
 }
+
+export default MainPage;
